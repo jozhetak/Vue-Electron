98017db7b29f17632c9e3554c06e9ef3e3e4679d
Jose Diestra Melendez <jozhetak@bitbucket.org>
2018-11-24 12:41:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue-Electron-Store
+Vue.js desktop application which is a redesigned app store for viewing and download apps.
+
+Technologies used:<br>
+Vue.js<br>
+Electron<br>
+Tailwind CSS<br>
+Vuex<br>
+Vue Router<br>
---
+++ vue-electron-store/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app"
+    style="-webkit-app-region: drag"
+    class="font-sans flex min-h-screen">
+    <sidebar></sidebar>
+    <Home />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue';
+import Home from './views/Home.vue';
+
+export default {
+  components: {
+    Sidebar,
+    Home
+  }
+}
+</script>
+
+<style src="./assets/main.css">
